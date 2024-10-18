08077aa802539ce1be8fcd77b6611fe2001bd6fb
Krisztián Hozák <business.frimex@gmail.com>
2024-10-18 14:47:42 +0200

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # Create lib directory
-mkdir -p NHF_Farm/lib/SDL2
-mkdir -p NHF_Farm/lib/SDL2_image
+mkdir -p lib/SDL2
+mkdir -p lib/SDL2_image
 
 # Download SDL2
 curl -L -o SDL2.zip https://www.libsdl.org/release/SDL2-2.28.4.zip
-unzip -q SDL2.zip -d NHF_Farm/lib/SDL2
+mkdir -p lib/temp
+unzip -q SDL2.zip -d lib/temp -vvv
+mv lib/temp/SDL2-2.28.4/* lib/SDL2
+rmdir lib/temp/SDL2-2.28.4
 rm SDL2.zip
 
 # Download SDL2_image
 curl -L -o SDL2_image.zip https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.6.3.zip
-unzip -q SDL2_image.zip -d NHF_Farm/lib/SDL2_image
+unzip -q SDL2_image.zip -d lib/temp
+mv lib/temp/SDL2_image-2.6.3/* lib/SDL2_image
+rmdir lib/temp/SDL2_image-2.6.3
+rmdir temp
 rm SDL2_image.zip
 
 echo "SDL2 és SDL2_image sikeresen telepítve!"
