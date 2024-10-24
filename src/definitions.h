0b67f2eb483d5bcb546d78dbcf9f2951fc539245
Krisztián Hozák <pistahozak@gmail.com>
2024-10-24 17:53:13 +0200

    Specifikáció hozzáadása

--- src/definitions.h
+++ src/definitions.h
@@ -2,8 +2,8 @@
 #define TILE_SIZE 16
 #define MAP_WIDTH 80
 #define MAP_HEIGHT 80
-#define screen_x 1200
-#define screen_y 800
+#define screen_x 1600
+#define screen_y 1200
 #define MAX_TILES 16
 #define SCALINGFACTOR 4
 #define TARGET_FPS 999
