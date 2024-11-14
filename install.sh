fbb5e68977f9f742730f189d6d500c71df8951e9
Christian Hozak <39135018+Chri6s@users.noreply.github.com>
2024-11-14 15:12:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,32 @@
-#!/bin/bash
+@echo off
 
-# Create lib directory
-mkdir -p lib/SDL2
-mkdir -p lib/SDL2_image
+REM Create lib directory
+mkdir lib\SDL2
+mkdir lib\SDL2_image
+mkdir lib\SDL2_ttf
+mkdir lib\temp
 
-# Download SDL2
-curl -L -o SDL2.zip https://github.com/libsdl-org/SDL/releases/download/release-2.30.8/SDL2-devel-2.30.8-VC.zip
-mkdir -p lib/temp
-unzip -q SDL2.zip -d lib/temp
-mv lib/temp/SDL2-2.30.8/* lib/SDL2
-rm SDL2.zip
+REM Download SDL2
+powershell -Command "Invoke-WebRequest -Uri https://github.com/libsdl-org/SDL/releases/download/release-2.30.8/SDL2-devel-2.30.8-VC.zip -OutFile SDL2.zip"
+powershell -Command "Expand-Archive -Path SDL2.zip -DestinationPath lib\temp"
+xcopy /E /I lib\temp\SDL2-2.30.8\* lib\SDL2
+del SDL2.zip
 
-# Download SDL2_image
-curl -L -o SDL2_image.zip https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.2/SDL2_image-devel-2.8.2-VC.zip
-unzip -q SDL2_image.zip -d lib/temp
-mv lib/temp/SDL2_image-2.8.2/* lib/SDL2_image
-rmdir lib/temp/SDL2_image-2.8.2
-rm SDL2_image.zip
+REM Download SDL2_image
+powershell -Command "Invoke-WebRequest -Uri https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.2/SDL2_image-devel-2.8.2-VC.zip -OutFile SDL2_image.zip"
+powershell -Command "Expand-Archive -Path SDL2_image.zip -DestinationPath lib\temp"
+xcopy /E /I lib\temp\SDL2_image-2.8.2\* lib\SDL2_image
+rmdir /S /Q lib\temp\SDL2_image-2.8.2
+del SDL2_image.zip
 
-# Download SDL2_ttf
-curl -L -o SDL2_ttf.zip https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.22.0/SDL2_ttf-devel-2.22.0-VC.zip
-unzip -q SDL2_ttf.zip -d lib/temp
-mv lib/temp/SDL2_ttf-2.22.0/* lib/SDL2_ttf
-rmdir lib/temp/SDL2_ttf-2.22.0
-rmdir temp
-rm SDL2_ttf.zip
+REM Download SDL2_ttf
+powershell -Command "Invoke-WebRequest -Uri https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.22.0/SDL2_ttf-devel-2.22.0-VC.zip -OutFile SDL2_ttf.zip"
+powershell -Command "Expand-Archive -Path SDL2_ttf.zip -DestinationPath lib\temp"
+xcopy /E /I lib\temp\SDL2_ttf-2.22.0\* lib\SDL2_ttf
+rmdir /S /Q lib\temp\SDL2_ttf-2.22.0
 
-echo "SDL2 és SDL2_image sikeresen telepítve!"
+REM Clean up
+rmdir /S /Q lib\temp
+del SDL2_ttf.zip
+
+echo SDL2, SDL2_image, and SDL2_ttf successfully installed!
